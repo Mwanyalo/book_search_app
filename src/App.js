7cbcf853f16c2b4e6ec9ebeea705ce5ff24bc996
Austine Mwanyalo <mwanyaloa@outlook.com>
2019-11-13 15:07:58 +0300

    imporoved ui

--- src/App.js
+++ src/App.js
@@ -1,96 +1,34 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Global, css } from '@emotion/core';
+import normalize from 'normalize.css';
+import { ThemeProvider } from 'emotion-theming';
+
+import theme from './components/theme';
+import SearchPage from './pages/SearchPage.js';
+import BookDetailPage from './pages/BookDetailPage';
+
+const NoMatchRoute = () => <div>404 Page</div>;
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [books, setBooks] = useState({ items: [] });
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const onInputChange = e => {
-    setSearchTerm(e.target.value);
-  };
-
-  let API_URL = `https://www.googleapis.com/books/v1/volumes`;
-
-  const fetchBooks = async () => {
-    // set loading Before API operation starts
-    setLoading(true);
-    setError(false);
-    try {
-      const result = await axios.get(`${API_URL}?q=${searchTerm}`);
-      setBooks(result.data);
-    } catch (error) {
-      setError(true);
-    }
-    // After API operation end
-    setLoading(false);
-  };
-
-  const onSubmitHandler = e => {
-    e.preventDefault();
-    fetchBooks();
-  };
-
-  const bookAuthors = authors => {
-    if (authors.length <= 2) {
-      authors = authors.join(' and ');
-    } else if (authors.length > 2) {
-      let lastAuthor = ' and ' + authors.slice(-1);
-      authors.pop();
-      authors = authors.join(', ');
-      authors += lastAuthor;
-    }
-    return authors;
-  };
-
   return (
-    <section>
-      <form onSubmit={onSubmitHandler}>
-        <label>
-          <span>Search for books</span>
-          <input
-            type='search'
-            placeholder='microservice, restful design, etc.,'
-            value={searchTerm}
-            onChange={onInputChange}
-            required
-          />
-          <button type='submit'>Search</button>
-        </label>
-        {error && (
-          <div style={{ color: `red` }}>
-            some error occurred, while fetching api
-          </div>
-        )}
-      </form>
-      {loading && (
-        <div style={{ color: `green` }}>
-          fetching books for "<strong>{searchTerm}</strong>"
-        </div>
-      )}
-      <ul>
-        {books.items.map((book, index) => {
-          return (
-            <li key={index}>
-              <div>
-                <img
-                  alt={`${book.volumeInfo.title} book`}
-                  src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-                />
-                <div>
-                  <h3>{book.volumeInfo.title}</h3>
-                  <p>{bookAuthors(book.volumeInfo.authors)}</p>
-                  <p>{book.volumeInfo.publishedDate}</p>
-                </div>
-              </div>
-              <hr />
-            </li>
-          );
-        })}
-      </ul>
-    </section>
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          ${normalize}
+          body {
+            background-color: #fafafa;
+          }
+        `}
+      />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={SearchPage} />
+          <Route path='/book/:bookId' exact component={BookDetailPage} />
+          <Route component={NoMatchRoute} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
